9fd915413729eacac43816535a3dda3bfd5e152a
atammana123 <aditya_tammana@yahoo.com>
2025-12-28 21:50:48 -0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ EMAIL_ADDRESS = os.environ.get('EMAIL')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 # For testing: I removed 4/18/2026 so it triggers an email immediately
-KNOWN_DATES = ["1/30/2026"] 
+KNOWN_DATES = ["1/30/2026", "4/18/2026"]
 
 def check_availability():
     print(f"DEBUG: Starting scraper. Looking for: Classic and Contemporary Sauces (Copia)")
