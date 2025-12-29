e5b5c4108d4cbe269784751b4411363671bb2192
atammana123 <aditya_tammana@yahoo.com>
2025-12-28 18:25:52 -0800

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,6 @@
+import os
+
+# Get these from GitHub Secrets later
+URL = "https://your-target-website.com"
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
